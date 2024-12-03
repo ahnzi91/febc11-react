d420e5d8be2b18c42c48d1ec27866dcf65d18c0d
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-03 10:20:40 +0900

    12월 3일 수업 : Button 컴포넌트에 Tailwind CSS 적용 완료

--- workspace/ch08-css/04-tailwind/src/components/Button.jsx
+++ workspace/ch08-css/04-tailwind/src/components/Button.jsx
@@ -1,16 +1,40 @@
 import PropTypes from "prop-types";
-import "./Button.css";
+
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   type: PropTypes.string,
-  color: PropTypes.oneOf(["blue", "red", "yellow", "gray"]), // 글자 색상
+  color: PropTypes.oneOf(["blue", "red", "black", "white"]), // 글자 색상
   bg: PropTypes.oneOf(["blue", "red", "yellow", "gray"]), // 배경 색상
+  size: PropTypes.oneOf(["sm", "md", "lg"]), // 버튼 크기
   onClick: PropTypes.func,
 };
 
-export default function Button({ children, type = "button", color, bg, onClick: clickHandler }) {
+export default function Button({ children, color = "black", bg = "gray", size = "md", ...rest }) {
+  // Tailwind CSS 사용
+  let bgColor = {
+    gray: "bg-gray-400",
+    blue: "bg-blue-500",
+    red: "bg-red-500",
+    yellow: "bg-yellow-500",
+  };
+
+  let textColor = {
+    black: "text-black",
+    white: "text-white",
+    blue: "text-blue-500",
+    red: "text-red-500",
+  };
+
+  let btnSize = {
+    sm: "text-sm px-2 py-1",
+    md: "text-base px-4 py-2",
+    lg: "text-lg px-6 py-3",
+  };
   return (
-    <button className={`button color-${bg}-${color}`} type={type} onClick={clickHandler}>
+    <button
+      className={`${bgColor[bg]} ${textColor[color]} ${btnSize[size]} m-1 rounded-md`}
+      {...rest}
+    >
       {children}
     </button>
   );
---
+++ workspace/ch08-css/04-tailwind/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // 어떤 파일을 Tailwind에 적용시킬 지 설정
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
