0d44ba2bc3f230323bda5ef7ba4de68fd4b4ff42
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-09 14:14:45 +0900

    12월 9일 월요일 : 수정 커밋

--- workspace/ch11-skeleton/lion-board/src/pages/board/Edit.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/Edit.jsx
@@ -1,11 +1,50 @@
+import InputError from "@components/InputError";
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
 export default function Edit() {
+  const { type, _id } = useParams();
+
+  const axios = useAxiosInstance();
+
+  const { data } = useQuery({
+    queryKey: ["posts", _id],
+    queryFn: () => axios.get(`/posts/${_id}`),
+    select: (res) => res.data,
+    staleTime: 1000 * 10,
+  });
+
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const queryClient = useQueryClient();
+
+  const updateItem = useMutation({
+    mutationFn: (formData) => axios.patch(`/posts/${_id}`, formData),
+    onSuccess: () => {
+      alert("게시물이 등록되었습니다.");
+      queryClient.invalidateQueries(["posts", _id]);
+      navigate(`/${type}/${_id}`);
+    },
+    onError: (err) => {
+      console.error(err);
+    },
+  });
+
   return (
     <main className="min-w-[320px] p-4">
       <div className="text-center py-4">
         <h2 className="text-2xl font-bold text-gray-700 dark:text-gray-200">게시글 수정</h2>
       </div>
       <section className="mb-8 p-4">
-        <form action="/info/1">
+        <form onSubmit={handleSubmit(updateItem.mutate)}>
           <div className="my-4">
             <label className="block text-lg content-center" htmlFor="title">
               제목
@@ -15,11 +54,10 @@ export default function Edit() {
               type="text"
               placeholder="제목을 입력하세요."
               className="w-full py-2 px-4 border rounded-md dark:bg-gray-700 border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
-              name="title"
-              value="좋은 소식이 있습니다."
+              {...register("title", { required: "제목은 필수입니다." })}
             />
 
-            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">제목은 필수 입니다.</p>
+            <InputError target={errors.title} />
           </div>
           <div className="my-4">
             <label className="block text-lg content-center" htmlFor="content">
@@ -30,11 +68,10 @@ export default function Edit() {
               rows="15"
               placeholder="내용을 입력하세요."
               className="w-full p-4 text-sm border rounded-lg border-gray-300 bg-gray-50 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              name="content"
-              defaultValue="좋은 소식을 가지고 왔습니다. 오늘 드디어 최종 면접을 합니다. 많이 응원해 주세요^^"
+              {...register("countent", { required: "내용은 필수입니다." })}
             />
 
-            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">내용은 필수입니다.</p>
+            <InputError target={errors.content} />
           </div>
           <hr />
           <div className="flex justify-end my-6">
@@ -44,12 +81,12 @@ export default function Edit() {
             >
               수정
             </button>
-            <a
-              href="/info/1"
+            <Link
+              to={`/${type}/${_id}`}
               className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
             >
               취소
-            </a>
+            </Link>
           </div>
         </form>
       </section>
