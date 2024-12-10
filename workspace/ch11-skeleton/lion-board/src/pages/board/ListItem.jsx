6078443feca54d32a3d2ad3ce61517fd7a8bad87
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-10 14:23:20 +0900

    12월 10일 수업 : Zustand 시작 전 커밋

--- workspace/ch11-skeleton/lion-board/src/pages/board/ListItem.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/ListItem.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 ListItem.propTypes = {
   item: PropTypes.shape({
     _id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
+    title: PropTypes.string,
     user: PropTypes.shape({
       name: PropTypes.string.isRequired,
     }),
@@ -16,19 +16,13 @@ ListItem.propTypes = {
 
 export default function ListItem({ item }) {
   return (
-    <>
-      <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
-        <td className="p-2 text-center">{item._id}</td>
-        <td className="p-2 truncate indent-4">
-          <Link to={`${item._id}`} className="cursor-pointer">
-            {item.title}
-          </Link>
-        </td>
-        <td className="p-2 text-center truncate">{item.user.name}</td>
-        <td className="p-2 text-center hidden sm:table-cell">{item.views}</td>
-        <td className="p-2 text-center hidden sm:table-cell">{item.repliesCount}</td>
-        <td className="p-2 truncate text-center hidden sm:table-cell">{item.createdAt}</td>
-      </tr>
-    </>
+    <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
+      <td className="p-2 text-center">{ item._id }</td>
+      <td className="p-2 truncate indent-4"><Link to={`${ item._id }`} className="cursor-pointer">{ item.title }</Link></td>
+      <td className="p-2 text-center truncate">{ item.user.name }</td>
+      <td className="p-2 text-center hidden sm:table-cell">{ item.views }</td>
+      <td className="p-2 text-center hidden sm:table-cell">{ item.repliesCount }</td>
+      <td className="p-2 truncate text-center hidden sm:table-cell">{ item.createdAt }</td>
+    </tr>
   );
 }
