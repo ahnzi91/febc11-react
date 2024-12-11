850ffc0812af40942dd5e2bcacd0e211ac19afe8
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-11 11:13:58 +0900

    Let's go

--- workspace/ch11-skeleton/lion-board-step1/src/pages/user/Signup.jsx
+++ workspace/ch11-skeleton/lion-board-step1/src/pages/user/Signup.jsx
@@ -10,9 +10,7 @@ export default function Signup() {
 
         <form action="/">
           <div className="mb-4">
-            <label className="block text-gray-700 dark:text-gray-200 mb-2" htmlFor="name">
-              이름
-            </label>
+            <label className="block text-gray-700 dark:text-gray-200 mb-2" htmlFor="name">이름</label>
             <input
               type="text"
               id="name"
@@ -23,9 +21,7 @@ export default function Signup() {
             <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">이름은 필수입니다.</p>
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 dark:text-gray-200 mb-2" htmlFor="email">
-              이메일
-            </label>
+            <label className="block text-gray-700 dark:text-gray-200 mb-2" htmlFor="email">이메일</label>
             <input
               type="email"
               id="email"
@@ -36,9 +32,7 @@ export default function Signup() {
             <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">이메일은 필수입니다.</p>
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 dark:text-gray-200 mb-2" htmlFor="password">
-              비밀번호
-            </label>
+            <label className="block text-gray-700 dark:text-gray-200 mb-2" htmlFor="password">비밀번호</label>
             <input
               type="password"
               id="password"
@@ -46,15 +40,11 @@ export default function Signup() {
               className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-orange-400 dark:bg-gray-700"
               name="password"
             />
-            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">
-              비밀번호는 필수입니다.
-            </p>
+            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">비밀번호는 필수입니다.</p>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 dark:text-gray-200 mb-2" htmlFor="attach">
-              프로필 이미지
-            </label>
+            <label className="block text-gray-700 dark:text-gray-200 mb-2" htmlFor="attach">프로필 이미지</label>
             <input
               type="file"
               id="attach"
@@ -66,18 +56,8 @@ export default function Signup() {
           </div>
 
           <div className="mt-10 flex justify-center items-center">
-            <button
-              type="submit"
-              className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-            >
-              회원가입
-            </button>
-            <Link
-              to="/"
-              className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-            >
-              취소
-            </Link>
+            <button type="submit" className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">회원가입</button>
+            <Link to="/" className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">취소</Link>
           </div>
         </form>
       </div>
